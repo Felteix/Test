ca867b734ee449b8f9606a2769f4c3b2e0d3b6c9
Felteix <Felix.Tenbruck@studmail.w-hs.de>
2017-11-20 22:23:19 +0100

    First commit

---
+++ GitDemo/src/Demo/main.java
@@ -0,0 +1,12 @@
+package Demo;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		test("Willkommen");
+	}
+	public static void test(String text){
+		System.out.println(text);
+	}
+}
